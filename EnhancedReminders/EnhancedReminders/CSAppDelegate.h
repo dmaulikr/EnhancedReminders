b8deff4bffb6913a6f4cb6e4e1471f9320a729d9
Adam LeBlanc <leblanca37@gmail.com>
2015-03-10 14:43:09 -0300

    This is the initial commit with the empy xcode project.

---
+++ EnhancedReminders/EnhancedReminders/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  EnhancedReminders
+//
+//  Created by iOS Developer on 2015-03-10.
+//  Copyright (c) 2015 UPEICS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
